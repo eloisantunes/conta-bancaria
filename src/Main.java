641394984b6ce8434e86eab2c8793e8012fa2feb
Eloísa Antunes <eloisantunesb@gmail.com>
2025-01-16 17:07:12 -0300

    desenvolvendo acoes do menu

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import java.sql.SQLOutput;
 import java.util.Scanner;
 
 public class Main {
@@ -28,7 +29,26 @@ public class Main {
         while (opcao != 4) {
             System.out.println(menu);
             opcao = leitura.nextInt();
-        }
 
+            if (opcao == 1) {
+                System.out.println("R$ " + saldo);
+            } else if (opcao == 2) {
+                System.out.println("Digite o valor para transferência");
+                double valor = leitura.nextDouble();
+                if (valor > saldo) {
+                    System.out.println("Saldo insuficiente");
+                } else {
+                    saldo -= valor;
+                    System.out.println("Saldo atual: R$ " + saldo);
+                }
+            } else if (opcao == 3) {
+                System.out.println("Valor recebido: R$ ");
+                double valor = leitura.nextDouble();
+                saldo += valor;
+                System.out.println("Saldo atual: R$ " + saldo);
+            } else if (opcao != 4) {
+                System.out.println("Opção inválida");
+            }
+        }
     }
 }
